b83250bacf13974443eac02399aee1f974a38854
Cong Yan <congy@cs.washington.edu>
2015-12-27 10:48:17 -0800

    add publify instr trace

--- applications/generate_dataflow.py
+++ applications/generate_dataflow.py
@@ -1,8 +1,9 @@
 import glob
 import os
 import sys
+merged_controllers = "merged_controllers"
 apppath = "./%s"%(sys.argv[1])
-controller_path = "%s/controllers/"%(apppath)
+controller_path = "%s/%s/"%(apppath, merged_controllers)
 model_path = "%s/models/"%(apppath)
 
 print "controller_path = %s"%controller_path
@@ -18,7 +19,7 @@ for root, dirs, files in os.walk(controller_path):
 			class_name = filename[n_begin+1:n_end]
 			print class_name
 			fname = os.path.join(root, filename)
-			k = fname.find("controllers")
+			k = fname.find("%s"%merged_controllers)
 			#des_file = "%s/%s.log"%(des_controller,class_name)
 			l = fname.find(".rb")
 			des_file = fname[:k-1] + "/dataflow" + fname[k-1:l] + ".log"
@@ -30,23 +31,23 @@ for root, dirs, files in os.walk(controller_path):
 			print "\tdes_file = %s"%des_file
 			os.system("/home/congy/jruby/bin/jrubyc %s >> %s"%(fname, des_file))
 
-for root, dirs, files in os.walk(model_path):
-	for filename in files:
-		if filename.endswith(".rb"):
-			print filename
-			n_begin = filename.rfind('/')
-			n_end = filename.rfind('.')
-			class_name = filename[n_begin+1:n_end]
-			print class_name
-			fname = os.path.join(root, filename)
-			k = fname.find("models")
-			#des_file = "%s/%s.log"%(des_controller,class_name)
-			l = fname.find(".rb")
-			des_file = fname[:k-1] + "/dataflow" + fname[k-1:l] + ".log"
-			d_ind = des_file.rfind('/')
-			dir_name = des_file[:d_ind]
-			print "dir_name = %s"%dir_name
-			if os.path.exists(dir_name) == False:
-				os.system("mkdir %s"%dir_name)
-			print "\tdes_file = %s"%des_file
-			os.system("/home/congy/jruby/bin/jrubyc %s >> %s"%(fname, des_file))
+#for root, dirs, files in os.walk(model_path):
+#	for filename in files:
+#		if filename.endswith(".rb"):
+#			print filename
+#			n_begin = filename.rfind('/')
+#			n_end = filename.rfind('.')
+#			class_name = filename[n_begin+1:n_end]
+#			print class_name
+#			fname = os.path.join(root, filename)
+#			k = fname.find("models")
+#			#des_file = "%s/%s.log"%(des_controller,class_name)
+#			l = fname.find(".rb")
+#			des_file = fname[:k-1] + "/dataflow" + fname[k-1:l] + ".log"
+#			d_ind = des_file.rfind('/')
+#			dir_name = des_file[:d_ind]
+#			print "dir_name = %s"%dir_name
+#			if os.path.exists(dir_name) == False:
+#				os.system("mkdir %s"%dir_name)
+#			print "\tdes_file = %s"%des_file
+#			os.system("/home/congy/jruby/bin/jrubyc %s >> %s"%(fname, des_file))
