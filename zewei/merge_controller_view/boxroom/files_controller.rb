3e162009af78649ae45066e9e8f478e5f971f7fe
ZeweiCHU <jasonchuzewei@gmail.com>
2015-12-28 13:53:38 -0600

    add merge controller view boxroom

---
+++ zewei/merge_controller_view/boxroom/files_controller.rb
@@ -0,0 +1,90 @@
+class FilesController < ApplicationController
+  before_action :require_existing_file, :only => [:show, :edit, :update, :destroy]
+  before_action :require_existing_target_folder, :only => [:new, :create]
+
+  before_action :require_create_permission, :only => [:new, :create]
+  before_action :require_read_permission, :only => :show
+  before_action :require_update_permission, :only => [:edit, :update]
+  before_action :require_delete_permission, :only => :destroy
+
+  # @file and @folder are set in require_existing_file
+  def show
+    send_file @file.attachment.path, :filename => @file.attachment_file_name
+  end
+
+  # @target_folder is set in require_existing_target_folder
+  def new
+    @file = @target_folder.user_files.build
+ content_for :title, t(:upload_file) 
+ content_for :title 
+ form_for [@target_folder, @file], :url => { :action => 'create' } do |f| 
+ hidden_field_tag :target_folder_id, @target_folder.id 
+ file_field_tag :attachment, :multiple => true, :name => 'user_file[attachment]' 
+ link_to t(:back), @target_folder 
+ end 
+ image_tag 'spinner.gif', :class => 'spinner' 
+ image_tag 'failed.png', :class => 'failed', :style => 'display:none;' 
+ image_tag 'tick.png', :class => 'tick', :style => 'display:none;' 
+ t(:exists_already) 
+
+  end
+
+  # @target_folder is set in require_existing_target_folder
+  def create
+    @file = @target_folder.user_files.create(permitted_params.user_file)
+    render :nothing => true
+  end
+
+  # @file and @folder are set in require_existing_file
+  def edit
+ content_for :title, t(:rename_file) 
+ content_for :title 
+ form_for @file, :url => { :action => 'update' } do |f| 
+ f.error_messages 
+ f.label :name 
+ f.text_field :attachment_file_name 
+ f.submit t(:save) 
+ link_to t(:back), @folder 
+ end 
+
+  end
+
+  # @file and @folder are set in require_existing_file
+  def update
+    if @file.update_attributes(permitted_params.user_file)
+      redirect_to edit_file_url(@file), :notice => t(:your_changes_were_saved)
+    else
+       content_for :title, t(:rename_file) 
+ content_for :title 
+ form_for @file, :url => { :action => 'update' } do |f| 
+ f.error_messages 
+ f.label :name 
+ f.text_field :attachment_file_name 
+ f.submit t(:save) 
+ link_to t(:back), @folder 
+ end 
+
+    end
+  end
+
+  # @file and @folder are set in require_existing_file
+  def destroy
+    @file.destroy
+    redirect_to @folder
+  end
+
+  def exists
+    @file = UserFile.new(:attachment_file_name => params[:name].gsub(RESTRICTED_CHARACTERS, '_'))
+    @file.folder_id = params[:folder]
+    render :json => !@file.valid?
+  end
+
+  private
+
+  def require_existing_file
+    @file = UserFile.find(params[:id])
+    @folder = @file.folder
+  rescue ActiveRecord::RecordNotFound
+    redirect_to Folder.root, :alert => t(:already_deleted, :type => t(:this_file))
+  end
+end
