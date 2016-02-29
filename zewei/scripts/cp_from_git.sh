cf424704b4e27ccfe60090e9ce2a7c6a0fd90eee
basevm <jasonchuzewei@gmail.com>
2016-02-29 10:39:37 -0800

    add cp git

---
+++ zewei/scripts/cp_from_git.sh
@@ -0,0 +1,3 @@
+cp ../ormanalysis/zewei/view_controller_analysis/read_files.rb .
+cp ../ormanalysis/zewei/view_controller_analysis/data_structure.rb .
+cp ../ormanalysis/zewei/view_controller_analysis/helper.rb .
---
+++ zewei/scripts/cp_to_git.sh
@@ -0,0 +1,3 @@
+cp read_files.rb ../ormanalysis/zewei/view_controller_analysis/
+cp data_structure.rb ../ormanalysis/zewei/view_controller_analysis/
+cp helper.rb ../ormanalysis/zewei/view_controller_analysis/
