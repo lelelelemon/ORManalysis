ed3f629d8effd388ce36a45c4fda7b29dbb749a4
Cong Yan <congy@cs.washington.edu>
2016-03-30 12:12:02 -0700

    add forem

---
+++ applications/forem/clean.py
@@ -0,0 +1,12 @@
+import os
+for root, dirs, files in os.walk("./"):
+	for filename in files:
+		if filename.endswith(".rb") and "schema.rb" not in filename:
+			fname = os.path.join(root, filename)
+			f = open(fname, 'r')
+			filedata = f.read()
+			newdata = filedata
+			newdata = newdata.replace("Forem::","")
+			f = open(fname, "w+")
+			f.write(newdata)
+			f.close()
