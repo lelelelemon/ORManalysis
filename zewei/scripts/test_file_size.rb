591646370d9a420662fdfb38d90e9fed27ee74df
basevm <jasonchuzewei@gmail.com>
2016-04-05 09:15:05 -0500

    add test file size

--- zewei/scripts/test_file_size.rb
+++ zewei/scripts/test_file_size.rb
@@ -5,7 +5,8 @@ total = 0
 empty = 0
 Dir.glob(ARGV[0] + "**/*") do |item|
   next if not item.end_with?".rb"
-  if File.zero?(item)
+  _item = item.gsub(".erb.rb"){".haml"}
+  if File.zero?(item) and not File.zero?(_item)
     puts item
     empty += 1
   end
