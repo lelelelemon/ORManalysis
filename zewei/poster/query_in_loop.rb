969f5c13b07b444b4d889e4898983253235f0c4a
Zewei Chu <jasonchuzewei@gmail.com>
2016-04-17 20:36:29 -0500

    update query in loop

--- zewei/poster/query_in_loop.rb
+++ zewei/poster/query_in_loop.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
   def batch_delete
     params.each do |k, v|
+      message = Message.where(:short_id => k[1])
       if message.author_user_id == @user.id
         message.deleted_by_author = true
       end
