3333c08201821e7b19737169851b3b021e738386
Cong Yan <congy@cs.washington.edu>
2016-12-03 12:22:58 -0800

    minor

--- controller_model_analysis/compute_redundant_rows.rb
+++ controller_model_analysis/compute_redundant_rows.rb
@@ -4,13 +4,16 @@ def compute_query_only_used_for_queries
 	$node_list.each do |n|
 		if n.isReadQuery?
 			qn_list = traceforward_data_dep(n)
-			only_to_query = true
+			only_to_query = false
 			test2 = true
 			#This kind of remove the cases like C.where().group(), so C.where() will not be considered
 			if n.getDataflowEdges.length == 1 and n.getDataflowEdges[0].getFromNode.getInstr.isQuery
 				test2 = false
 			end
 			qn_list.each do |n1|
+				if n1.isQuery?
+					only_to_query = true
+				end
 				if n1.getDataflowEdges.length == 0
 					if n1.isBranch? or n1.getInView or isValidationSink(n1) or isCacheSink(n1)
 						only_to_query = false
@@ -20,7 +23,7 @@ def compute_query_only_used_for_queries
 					end
 				end
 			end
-			if test2
+			if test2 and only_to_query
 				if n.getNonViewClosureStack.length > 0
 					@in_loop += 1
 				end
