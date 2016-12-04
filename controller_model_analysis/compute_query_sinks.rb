1f72f6eb13d32be2641fcfdaf324f332d116ba9c
Guna Prasaad <gunaprsd@gmail.com>
2016-12-03 20:27:19 -0800

    modified query_sinks file

--- controller_model_analysis/compute_query_sinks.rb
+++ controller_model_analysis/compute_query_sinks.rb
@@ -1,6 +1,6 @@
 require 'active_support/core_ext/string'
 def analyse_query_sinks
-	find_interesting_nodes
+	compute_query_only_used_for_queries
 end
 
 def get_sinks_as_xml(node)
@@ -23,15 +23,143 @@ def get_sinks_as_xml(node)
   end
 end
 
+def find_forward_query_functions(node, query_blob)
+	continue = true
+	while continue
+		if node.getDataflowEdges.length == 1
+			next_node = node.getDataflowEdges[0].getToNode
+			if next_node != nil and not $query_blob_visited_nodes.include?next_node.getIndex and next_node.isQuery?
+				$query_blob_visited_nodes.push(next_node.getIndex)
+				#puts $query_blob_visited_nodes.join(" ")
+				query_blob.insert(0, next_node)
+				node = next_node
+			else
+				continue = false
+			end
+		else
+			continue = false
+		end
+	end
+	return query_blob
+end
+
+def find_backward_query_functions(node, query_blob)
+	continue = true
+	while continue
+		if node.getBackwardEdges.length == 1
+			prev_node = node.getBackwardEdges[0].getFromNode
+			if prev_node != nil and not $query_blob_visited_nodes.include?prev_node.getIndex and prev_node.isQuery?
+				$query_blob_visited_nodes.push(prev_node.getIndex)
+				#puts $query_blob_visited_nodes.join(" ")
+				query_blob.push(prev_node)
+				node = prev_node
+			else
+				continue = false
+			end
+		else
+			continue = false
+		end
+	end
+	return query_blob
+end
+
+def find_query_blob(node)
+	if not $query_blob_visited_nodes.include?node.getIndex
+		$query_blob_visited_nodes.push(node.getIndex)
+		#puts $query_blob_visited_nodes.join(" ")
+		query_blob = [node]
+		query_blob = find_forward_query_functions(node, query_blob)
+		query_blob = find_backward_query_functions(node, query_blob)
+		$query_stmts.push(query_blob)
+	end
+end
+
+def find_all_query_blobs
+	$query_blob_visited_nodes = []
+	$query_stmts = []
+	$node_list.each do |n|
+		if n.isQuery? and not $query_blob_visited_nodes.include?n
+			find_query_blob(n)
+		end
+	end
+
+	info_file = File.open("#{$app_dir}/querySinksAnalysis.xml", "a")
+	$query_stmts.each do |stmt|
+		content = ""
+		stmt.each do |n|
+			content += get_xml("instruction", n.getInstr.toString, attributes: [["node_index", n.getIndex.to_s]], escape_content: true)
+		end
+
+		start_node = stmt[0]
+		end_node = stmt[stmt.length - 1]
+		if start_node.getBackwardEdges.length > 0
+			start_node.getBackwardEdges.each do |edge|
+				prev_node = edge.getFromNode
+				if prev_node != nil
+					content = get_xml("prev_instruction", prev_node.getInstr.toString, attributes: [["node_index", prev_node.getIndex.to_s]], escape_content: true) + content
+				end
+			end
+		end
+		if end_node.getDataflowEdges.length > 0
+			end_node.getDataflowEdges.each do |edge|
+				next_node = edge.getToNode
+				if next_node != nil
+					content += get_xml("next_instruction", next_node.getInstr.toString, attributes: [["node_index", next_node.getIndex.to_s]], escape_content:true)
+				end
+			end
+		end
+		info_file.puts get_xml("query", content, attributes:[["inst_count", stmt.length.to_s]])
+	end
+	info_file.close
+end
+
 def find_interesting_nodes
+	content =""
+	count = 0
+	$node_list.each do |n|
+		if n.getInstr.instance_of?AttrAssign_instr
+			node_content = ""
+			if n.getBackwardEdges.length == 1
+				prev_node = n.getBackwardEdges[0].getFromNode
+
+				if prev_node != nil and prev_node.isQuery?
+					count += 1
+					node_content = trace_data_dependency_till_sink(n, n, 0, [], is_root:true)
+					# node_content += get_xml("instruction", n.getInstr.toString, attributes: [["node_index", n.getIndex.to_s]], escape_content: true)
+					# count += 1
+					# next_nodes_content = ""
+					# n.getDataflowEdges.each do |edge|
+					# 	next_node = edge.getToNode
+					# 	next_nodes_content += get_xml("instruction", next_node.getInstr.toString, attributes: [["node_index", next_node.getIndex.to_s]], escape_content: true)
+					# end
+					# node_content += get_xml("forward_edges", next_nodes_content, attributes: [["count", n.getDataflowEdges.length]])
+				end
+				content += get_xml("assign", node_content)
+			end
+		end
+	end
+
+	if(content != "")
+		info_file = File.open("#{$app_dir}/querySinksAnalysis.xml", "a")
+		info_file.puts get_xml("action", content, attributes:[["class", $start_class.to_s], ["function", $start_function.to_s], ["count", count.to_s]])
+		info_file.close
+	end
+end
+
+def find_interesting_nodes_old
   $queries_and_sinks = Hash.new
   content = ""
   count = 0
+
 	$node_list.each do |n|
 		if n.isQuery?
-      $queries_and_sinks.clear
-      content += trace_data_dependency_till_sink(n, n, 0, [], is_root:true)
-      count += 1
+			if n.getBackwardEdges.length == 1 and n.getBackwardEdges[0].getFromNode.isQuery?
+				#ignore the exploration here
+			else
+	      $queries_and_sinks.clear
+	      content += trace_data_dependency_till_sink(n, n, 0, [], is_root:true)
+	      count += 1
+			end
 		end
 	end
 
@@ -43,10 +171,10 @@ def find_interesting_nodes
 	end
 end
 
-def trace_data_dependency_till_sink(root_node, node, distance, visited_nodes, is_root: false)
+def trace_data_dependency_till_sink(root_node, node, distance, visited_nodes, as_node:true, is_root: false)
   node_index = node.getIndex
   instruction_content = node.getInstr.toString
-	node_content = get_xml("instruction", instruction_content.to_s, escape_content: true)
+	node_content = get_xml("instruction", instruction_content.to_s, attributes: [["node_index", node.getIndex.to_s]], escape_content: true)
   is_query = node.isQuery?
 
   #check if the node is a query and explore children only if root
@@ -61,8 +189,15 @@ def trace_data_dependency_till_sink(root_node, node, distance, visited_nodes, is
   sink = false
   if explore
     children_count = node.getDataflowEdges.length
-    if children_count > 0 and \
-      not visited_nodes.include?(node.getIndex)
+		# if children_count == 1 and node.getDataflowEdges[0].getToNode.isQuery?
+		# 	#identify stacked up query functions
+		# 	visited_nodes.push(node.getIndex)
+		# 	to_node = node.getDataflowEdges[0].getToNode
+		# 	node_content += trace_data_dependency_till_sink(root_node, to_node, distance, visited_nodes, as_node: false)
+		# 	visited_nodes.delete(node.getIndex)
+		# 	sink = false
+    if children_count > 0 and not visited_nodes.include?(node.getIndex)
+			#traverse overall graph, if not a stacked up query function
       children_content = ""
       visited_nodes.push(node.getIndex)
       node.getDataflowEdges.each do |edge|
@@ -71,7 +206,7 @@ def trace_data_dependency_till_sink(root_node, node, distance, visited_nodes, is
           children_content += trace_data_dependency_till_sink(root_node, child_node, distance + 1, visited_nodes)
         end
       end
-      #node_content += get_xml("children", children_content, attributes: [["count", children_count]])
+      node_content += get_xml("children", children_content, attributes: [["count", children_count]])
       visited_nodes.delete(node.getIndex)
     elsif children_count == 0 and not visited_nodes.include?(node.getIndex)
       sink = true
@@ -80,20 +215,73 @@ def trace_data_dependency_till_sink(root_node, node, distance, visited_nodes, is
     sink = true
   end
 
-  if sink and root_node.getIndex != node.getIndex
-    if $queries_and_sinks.has_key?root_node
-			$queries_and_sinks[root_node] << node
-		else
-			$queries_and_sinks[root_node] = [node]
-		end
-  end
+  # if sink and root_node.getIndex != node.getIndex
+  #   if $queries_and_sinks.has_key?root_node
+	# 		$queries_and_sinks[root_node] << node
+	# 	else
+	# 		$queries_and_sinks[root_node] = [node]
+	# 	end
+  # end
 
-  if is_root
-    node_content += get_sinks_as_xml(root_node)
-    return get_xml("query", node_content.to_s, attributes: [["index", node.getIndex.to_s], ["distance", distance.to_s]])
-  else
-    return get_xml("node", node_content.to_s, attributes: [["index", node.getIndex.to_s], ["distance", distance.to_s]])
-  end
+	if as_node
+	  if is_root
+	    #node_content += get_sinks_as_xml(root_node)
+	    return get_xml("query", node_content.to_s, attributes: [["distance", distance.to_s]])
+	  else
+	    return get_xml("node", node_content.to_s, attributes: [["distance", distance.to_s]])
+	  end
+	else
+		return node_content.to_s
+	end
+end
+
+def compute_query_only_used_for_queries
+	count = 0
+	loop_count = 0
+	content = ""
+	$node_list.each do |n|
+		is_in_loop = false
+		if n.isReadQuery?
+			qn_list = traceforward_data_dep(n)
+			only_to_query = true
+			ignore = false
+
+			if n.getDataflowEdges.length == 1 and n.getDataflowEdges[0].getFromNode.getInstr.isQuery
+				#remove the cases like C.where().group(), so C.where() will not be considered
+				ignore = true
+			end
+
+			qn_list.each do |n1|
+				if n1.isQuery?
+					only_to_query = true
+				end
+
+				if n1.getDataflowEdges.length == 0
+					if n1.isBranch? or n1.getInView or isValidationSink(n1) or isCacheSink(n1)
+						only_to_query = false
+					end
+					if sinkIgnore(n1) == false
+						ignore = true
+					end
+				end
+			end
+
+			if not ignore and only_to_query
+				if n.getNonViewClosureStack.length > 0
+					is_in_loop = true
+					loop_count += 1
+				end
+				count += 1
+				content += get_xml("instruction", n.getInstr.toString, attributes:[["index", n.getIndex], ["in_loop", is_in_loop.to_s]], escape_content:true)
+			end
+		end
+	end
+
+	if(content != "")
+		info_file = File.open("#{$app_dir}/querySinksAnalysis.xml", "a")
+		info_file.puts get_xml("action", content, attributes:[["class", $start_class.to_s], ["function", $start_function.to_s], ["count", count.to_s], ["loop_count", loop_count.to_s]])
+		info_file.close
+	end
 end
 
 def get_app_name_from_dir(app_dir)
